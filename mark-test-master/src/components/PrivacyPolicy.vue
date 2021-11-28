5e0b6908bd609ddeb1f992aafd1fa389e95a278d
Wanjiku25 <93423651+Wanjiku25@users.noreply.github.com>
2021-11-28 04:02:29 -0600

    Add files via upload

---
+++ mark-test-master/src/components/PrivacyPolicy.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="policy-wrap" aria-labelledby="priv-pol-heading">
+    <h1 id="priv-pol-heading">Privacy Policy</h1>
+    <p class="py-12">Information about privacy that you should read in detail.</p>
+    <v-btn class="primary" @click="confirm">OK, I read it, sheesh</v-btn>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: "PrivacyPolicy",
+    methods: {
+      confirm() {
+        this.$emit('confirm')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.policy-wrap {
+  border: 1px solid rgba(0,0,0,0.4);
+  padding: 4rem;
+}
+</style>
